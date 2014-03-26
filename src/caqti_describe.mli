b30638863f1a4452a6129278ee40fabe9fdf0d04
Petter Urkedal <paurkedal@gmail.com>
2014-03-26 08:29:27 +0100

    Fix doc header for Caqti_describe.

--- src/caqti_describe.mli
+++ src/caqti_describe.mli
@@ -14,7 +14,7 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
-(** Type definitions. *)
+(** Types and utility functions for query descriptions. *)
 
 type typedesc =
   [ `Bool

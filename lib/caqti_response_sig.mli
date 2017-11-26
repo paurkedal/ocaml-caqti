d7c38a5dd46fbb1686c97d7b4c04ad765e190bc0
Petter A. Urkedal <paurkedal@gmail.com>
2017-11-26 10:09:56 +0100

    Open up error types in Caqti_response_sig.S.

--- lib/caqti_response_sig.mli
+++ lib/caqti_response_sig.mli
@@ -28,34 +28,34 @@ module type S = sig
   (** {2 Result inspection} *)
 
   val returned_count :
-    ('b, 'm) t -> (int, [Caqti_error.retrieve | `Unsupported]) result io
+    ('b, 'm) t -> (int, [> Caqti_error.retrieve | `Unsupported]) result io
   (** [returned_count resp] is the number of rows returned by [resp].  This
       function may not be available for all databases. *)
 
   val affected_count :
-    ('b, 'm) t -> (int, [Caqti_error.retrieve | `Unsupported]) result io
+    ('b, 'm) t -> (int, [> Caqti_error.retrieve | `Unsupported]) result io
   (** [affected_count resp] is the number of rows affected by the updated the
       produced [resp].  This function may not be available for all databases. *)
 
   (** {2 Result retrieval} *)
 
   val exec :
-    (unit, [< `Zero]) t -> (unit, Caqti_error.retrieve) result io
+    (unit, [< `Zero]) t -> (unit, [> Caqti_error.retrieve]) result io
   (** [exec resp] checks that [resp] succeeded with no result rows. *)
 
   val find :
-    ('b, [< `One]) t -> ('b, Caqti_error.retrieve) result io
+    ('b, [< `One]) t -> ('b, [> Caqti_error.retrieve]) result io
   (** [find resp] checks that [resp] succeeded with a single row, and returns
       the decoded row. *)
 
   val find_opt :
-    ('b, [< `Zero | `One]) t -> ('b option, Caqti_error.retrieve) result io
+    ('b, [< `Zero | `One]) t -> ('b option, [> Caqti_error.retrieve]) result io
   (** [find_opt resp] checks that [resp] succeeded with at most one row, and
       returns the row if any. *)
 
   val fold :
     ('b -> 'c -> 'c) ->
-    ('b, 'm) t -> 'c -> ('c, Caqti_error.retrieve) result io
+    ('b, 'm) t -> 'c -> ('c, [> Caqti_error.retrieve]) result io
   (** [fold f resp] folds [f] over the decoded rows returned in [resp]. *)
 
   val fold_s :

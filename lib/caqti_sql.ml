6896189860e015a81cb77439c514953da548b6ee
Petter A. Urkedal <paurkedal@gmail.com>
2021-10-10 19:43:22 +0200

    Replace OCaml LGPL Linking Exception with LGPL-3.0 Linking Exception.

--- lib/caqti_sql.ml
+++ lib/caqti_sql.ml
@@ -3,7 +3,7 @@
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version, with the OCaml static compilation exception.
+ * option) any later version, with the LGPL-3.0 Linking Exception.
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -11,7 +11,8 @@
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * and the LGPL-3.0 Linking Exception along with this library.  If not, see
+ * <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.
  *)
 
 let bprint_sql_escaped buf s =
